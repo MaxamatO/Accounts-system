073ee1b10e2d2b0969258e3afe76143c9a1b76a2
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-06-05 16:20:46 +0200

    Fix: some frontend stuff

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <nav className='w-full h-24 flex-auto flex'>
-            <div className="h-full flex flex-1 justify-end gap-10 p-10">
+        <nav className='w-full max-h-24 relative flex-auto flex border-b-2 border-gray-800'>
+            <div className=" relative h-full flex flex-1 justify-end gap-10 p-10">
                 <Link href="/">Home</Link>
                 <Link href="https://github.com/MaxamatO" target="_blank">Git Hub</Link>
             </div>
--- src/app/createaccount/page.tsx
+++ src/app/createaccount/page.tsx
@@ -4,18 +4,41 @@ export default function Page() {
     return (
       <main className='h-full w-full flex flex-col'>
         <div className='flex h-full'>
-            <div className='flex h-full w-1/2 justify-center items-center border-white border '>
-              <div className="w-2/3 h-2/3 border">
+            <div className='flex h-full w-1/2 justify-center items-center border-r-2 border-gray-800'>
+              <div className="w-2/3 h-2/3">
                 <form action="" className="w-full h-full flex flex-col gap-16 place-content-center items-center">
-                  <input type="text" className="w-1/2 h-10" />
-                  <input type="text" className="w-1/2 h-10" />
-                  <button type="button" className="w-1/2 h-10 bg-gray-700">Log in</button>
+                  <div className="flex flex-col w-60 gap-1">
+                    <label htmlFor="">email</label>
+                    <input type="text" className="w-60 h-10 text-black" />
+                  </div>
+                  <div className="flex flex-col w-60 gap-1">
+                  <label htmlFor="">password</label>
+                  <input type="password" className="w-60 h-10 text-black" />
+                  </div>
+                  <button type="button" className="w-32  h-10 bg-gray-700">Log in</button>
                 </form>
               </div>
             </div>
-            <div className='relative flex justify-center items-center h-full w-1/2 border-red-200 border'> 
-              <div className="w-2/3 h-2/3 border">
-
+            <div className='relative flex justify-center items-center h-full w-1/2'> 
+              <div className="w-2/3 h-2/3">
+                <form action="" className="w-full h-full flex flex-col gap-16 place-content-center items-center">
+                  <div className="flex flex-col w-60 gap-1">
+                    <label htmlFor="">email</label>
+                    <input type="text" className="w-60 h-10 text-black" />
+                  </div>
+                  <div className="flex flex-col w-60 gap-16">
+                    <div className="flex  flex-col gap-1">
+                      <label htmlFor="">password</label>
+                      <input type="password" className="w-60 h-10 text-black" />
+                    </div>
+                    <div className="flex flex-col gap-1">
+                      <label htmlFor="">repeat password</label>
+                      <input type="password" className="w-60 h-10 text-black" />
+                    </div>
+                      
+                  </div>
+                  <button type="button" className="w-32  h-10 bg-gray-700">Register</button>
+                </form>
               </div>
             </div>
         </div>
