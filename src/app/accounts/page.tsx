7b5ac08ac4c4d697d09f23139a5a6045b85f32b0
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-06-07 18:00:02 +0200

    Add GET users

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -1,7 +1,24 @@
+"use client"
+import { useEffect, useState } from "react";
+import { fetchUsers } from "../../../utils/helper_functions"
+import { User } from "../../../types";
+
 export default function Page() {
+    const [users, setUsers] = useState<User[]>([]);
+
+    
+    useEffect(() => {
+        fetchUsers().then(res => setUsers(res));
+        console.log(users);
+        
+    }, []);
+    
+
     return (
         <>
-            {"users"}
+        {users.map((user) => {
+            <div>{user.email}</div>
+        })}
         </>
     )
 }
---
+++ src/app/api/users/route.ts
@@ -0,0 +1,14 @@
+ 
+import { NextApiRequest, NextApiResponse } from "next";
+import { User } from "../../../../types";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const DATABASE_URL = "https://jsonplaceholder.typicode.com/users"
+
+const prisma = new PrismaClient();
+
+export async function GET(){
+    const users: User[] = await prisma.user.findMany();
+    return NextResponse.json(users);
+}
