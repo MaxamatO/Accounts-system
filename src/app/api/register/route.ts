9420bf803fd529e42488a5710c48b66e0c67e0dc
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-06-13 15:56:09 +0200

    Refactor

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,19 +3,11 @@ import { PrismaClient } from "@prisma/client";
 import { User } from "../../../../types";
 import { NextResponse } from "next/server";
 import { ErrorMessage } from "../../../../types";
-import { ApiError } from "next/dist/server/api-utils";
 import { HttpCodes } from "../../../../utils/HttpErrors";
-import { NextApiResponse } from "next";
+import { ErrorTypes } from "../../../../utils/ErrorTypes";
+import { okCredentials } from "../../../../utils/helper_functions";
+import { prisma } from "../../../../utils/prismaClient";
 
-
-const prisma = new PrismaClient();
-
-function okCredentials(user: User, rePassword: string): ErrorMessage {
-    if(user.password === "" || user.email === "") return {errorType: "Fields cant be blank", ok: false};
-    if(user.password.length < 8 || user.password.length > 20) return {errorType: "Password must contain 8-20 characters", ok: false};
-    if(user.password !== rePassword) return {errorType: "Passwords are not equal", ok:false};
-    return {errorType: "None", ok: true};
-}
 
 export async function POST(req: Request) {
     const body = await req.json();
@@ -26,21 +18,20 @@ export async function POST(req: Request) {
         verified: false,
         role: "User"
     };
-    const areCredentialsOk: ErrorMessage = okCredentials(user, rePassword); 
+    const areCredentialsOk: ErrorMessage = await okCredentials(user, rePassword); 
     const errorMessage: ErrorMessage = {
         errorType: areCredentialsOk.errorType,
-        ok: areCredentialsOk.ok
+        ok: areCredentialsOk.ok,
+        errorText: areCredentialsOk.errorText
     }
     if(areCredentialsOk.ok === true){
         console.log(email, password);
-        const createUser = await prisma.user.create({data: user},);
-        console.log("im here");
-        
+        const createdUser = await prisma.user.create({data: user},);
         return NextResponse.json(errorMessage); 
     };
-    console.log("also here");
     return NextResponse.json({
-        statusText: errorMessage.errorType
+        statusText: errorMessage.errorText,
+        errorType: errorMessage.errorType
     }, {
         status: HttpCodes.unauthorized
     }
