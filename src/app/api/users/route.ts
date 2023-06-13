6872df1908c7076ef8a798ab1edf3ecf138b0a4b
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-06-13 16:44:51 +0200

    Add import

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,7 +1,7 @@
 "use server"
 import { User } from "../../../../types";
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../utils/prismaClient";
+import prisma  from "../../../../lib/prisma";
 
 export async function GET(){
     const users: User[] = await prisma.user.findMany();
