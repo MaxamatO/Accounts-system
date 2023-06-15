a9b10d0178e570347ad612ffa7c7e3ef2a121992
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-06-15 23:12:50 +0200

    create login endpoint

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,10 +1,29 @@
 import { NextResponse } from "next/server";
-import prisma  from "../../../../lib/prisma";
+import { ErrorMessage } from "../../../../types";
+import { okCredentialsLogin } from "../../../../utils/helper_functions";
+import { HttpCodes } from "../../../../utils/HttpErrors";
+import prisma from "../../../../lib/prisma";
+import { use } from "react";
+
+interface RequestBody {
+    email: string;
+    password: string;
+}
 
 export async function POST(req: Request) {
-    const body = await req.json();
-    console.log(body.password, body.email);
-    console.log(body);
+    const body: RequestBody = await req.json();
+    // const areCredentialsOk: ErrorMessage = await okCredentialsLogin(body.username, body.password); 
+    // const errorMessage: ErrorMessage = {
+    //     errorType: areCredentialsOk.errorType,
+    //     ok: areCredentialsOk.ok,
+    //     errorText: areCredentialsOk.errorText
+    // }
+    const user = await prisma.user.findFirst({where: {email: body.email}})
+
+    if(user && user.password === body.password) {
+        const {password, ...userWithoutPass} = user; 
+        return new Response(JSON.stringify(userWithoutPass));
+    }
     
-    return NextResponse.json("");
+    return new Response(JSON.stringify(null));
 }
