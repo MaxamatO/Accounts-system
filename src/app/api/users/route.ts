3db6d149db25ff9af261762b25b5ad06f0ca73a8
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-06-20 15:06:17 +0200

    Secure API route

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,8 +2,22 @@
 import { User } from "../../../../types";
 import { NextResponse } from "next/server";
 import prisma  from "../../../../lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route"
+import { Roles } from "../../../../utils/Roles";
+import { HttpCodes } from "../../../../utils/HttpErrors";
 
 export async function GET(){
-    const users: User[] = await prisma.user.findMany();
-    return NextResponse.json(users);
+    const session = await getServerSession(authOptions);
+    if(session && session.user.role === Roles.ADMIN) {
+        const users: User[] = await prisma.user.findMany();
+        return NextResponse.json(users);
+    }
+    return NextResponse.json({
+        message: "Unauthorized"
+    },
+    {
+        status: HttpCodes.unauthorized
+    }
+    )
 }
