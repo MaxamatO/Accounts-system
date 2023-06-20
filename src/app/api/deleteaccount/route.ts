928f7c8955f4d5879bb0cc7efd95001d17e0db14
Maksymilian Pierchała <maksdfg123@gmail.com>
2023-06-20 17:21:27 +0200

    Add delete endpoint

---
+++ src/app/api/deleteaccount/route.ts
@@ -0,0 +1,61 @@
+"use server"
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { Roles } from "../../../../utils/Roles";
+import prisma from "../../../../lib/prisma";
+import { NextResponse } from "next/server";
+import { HttpCodes } from "../../../../utils/HttpErrors";
+
+interface RequestBody {
+    email: string;
+}
+
+export async function DELETE(req: Request) {
+    const session = await getServerSession(authOptions);
+    
+    if(!session || session.user.role!==Roles.ADMIN) {
+        return NextResponse.json({
+            message: "Unauthorized"
+        },
+        {
+            status: HttpCodes.unauthorized
+        }
+        )
+    }
+
+    const {searchParams} = new URL(req.url);
+    const email = searchParams.get('id');
+    console.log(email);
+    
+    if(!email) return NextResponse.json({
+        message: "No email was provided"
+    },
+    {
+        status: HttpCodes.bad_request
+    }
+    )
+    const exists = !!await prisma.user.findUnique({where: {email: email}});
+    console.log(exists);
+    
+
+    if(!exists) {
+        return NextResponse.json({
+            message: "User does not exist"
+        },
+        {
+            status: HttpCodes.bad_request
+        }
+        )
+    }
+
+    const result = await prisma.user.delete({where: {email: email}});
+    return NextResponse.json({
+        user: result,
+        message: "Deleted"
+    },
+    {
+        status: HttpCodes.success
+    }
+    )
+    
+}
